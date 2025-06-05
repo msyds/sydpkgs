1390585d71a32f2c0080812ff7dff6e7098d6088
Madeleine Sydney Ślaga <msyds@deertopia.net>
2025-06-05 05:47:10 -0600

    dolphin-mpn: init

---
+++ pkgs/dolphin-mpn/default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.dolphin-emu.overrideAttrs (prev: final: {
+  pname = "Dolphin-MPN";
+  version = "0712d84";
+  src = pkgs.fetchFromGitHub {
+    owner = "MarioPartyNetplay";
+    repo = "Dolphin-MPN";
+    rev = "0712d84c74f696791acf02df4c22e88741665da6";
+    hash = "sha256-dhcLJCQzd7a0l3AVFFVxfQ95sVql5N4e63xdvl7h9uc=";
+    fetchSubmodules = true;
+  };
+})
